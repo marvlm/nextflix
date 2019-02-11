ad7786a0993518f632749ff90d1e0301fddeefff
Marvin <marv.wi3@gmail.com>
2019-02-11 16:39:10 +0100

    Se agrego el logo del app como un atributo de la clase Config

--- lib/src/resources/config.dart
+++ lib/src/resources/config.dart
@@ -3,9 +3,11 @@ class Config {
   var _youtubeKey = 'AIzaSyBCfP7rIsyrywYhfuRJ4KUIDweeH7mbkSI';
   var _imageUrl = 'https://image.tmdb.org/t/p/w500';
   var _apiUrl = 'http://api.themoviedb.org/3';
+  var _logo = 'images/NextFlix-logo-horizontal.png';
 
   String get apiKey => _apiKey;
   String get youtubeKey => _youtubeKey;
   String get imageUrl => _imageUrl;
   String get apiUrl => _apiUrl;
+  String get logo => _logo;
 }
