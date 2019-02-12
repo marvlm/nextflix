da06b8edc0742d5a2254d14b6ca0fe4d24fcd9b5
Marvin <marv.wi3@gmail.com>
2019-02-12 18:18:47 +0100

    Tests - Se agregaron tests para todos los metodos del api provider (cargar lista de peliculas y series de tv)

--- test/api_provider_test.dart
+++ test/api_provider_test.dart
@@ -5,33 +5,105 @@ import 'package:http/testing.dart';
 import 'package:nextflix_test1/src/resources/tmdb_api_provider.dart';
 
 void main() {
-  test('Testing the network call', () async {
-    final apiProvider = TMDBApiProvider();
-    apiProvider.client = MockClient((request) async {
-      final mapJson = {
-        "results": [
-          {
-            "vote_count": 1031,
-            "id": 450465,
-            "video": false,
-            "vote_average": 6.9,
-            "title": "Glass",
-            "popularity": 427.352,
-            "poster_path": "\/svIDTNUoajS8dLEo7EosxvyAsgJ.jpg",
-            "original_language": "en",
-            "original_title": "Glass",
-            "genre_ids": [53, 9648, 18],
-            "backdrop_path": "\/lvjscO8wmpEbIfOEZi92Je8Ktlg.jpg",
-            "adult": false,
-            "overview":
-                "In a series of escalating encounters, security guard David Dunn uses his supernatural abilities to track Kevin Wendell Crumb, a disturbed man who has twenty-four personalities. Meanwhile, the shadowy presence of Elijah Price emerges as an orchestrator who holds secrets critical to both men.",
-            "release_date": "2019-01-16"
-          }
-        ]
-      };
-      return Response(json.encode(mapJson), 200);
+  final moviesJson = {
+    "results": [
+      {
+        "vote_count": 1031,
+        "id": 450465,
+        "video": false,
+        "vote_average": 6.9,
+        "title": "Glass",
+        "popularity": 427.352,
+        "poster_path": "\/svIDTNUoajS8dLEo7EosxvyAsgJ.jpg",
+        "original_language": "en",
+        "original_title": "Glass",
+        "genre_ids": [53, 9648, 18],
+        "backdrop_path": "\/lvjscO8wmpEbIfOEZi92Je8Ktlg.jpg",
+        "adult": false,
+        "overview":
+            "In a series of escalating encounters, security guard David Dunn uses his supernatural abilities to track Kevin Wendell Crumb, a disturbed man who has twenty-four personalities. Meanwhile, the shadowy presence of Elijah Price emerges as an orchestrator who holds secrets critical to both men.",
+        "release_date": "2019-01-16"
+      }
+    ]
+  };
+
+  final tvsJson = {
+    "results": [
+      {
+        "original_name": "Breaking Bad",
+        "genre_ids": [18],
+        "name": "Breaking Bad",
+        "popularity": 88.59,
+        "origin_country": ["US"],
+        "vote_count": 3076,
+        "first_air_date": "2008-01-20",
+        "backdrop_path": "/eSzpy96DwBujGFj0xMbXBcGcfxX.jpg",
+        "original_language": "en",
+        "id": 1396,
+        "vote_average": 8.4,
+        "overview":
+            "When Walter White, a New Mexico chemistry teacher, is diagnosed with Stage III cancer and given a prognosis of only two years left to live. He becomes filled with a sense of fearlessness and an unrelenting desire to secure his family's financial future at any cost as he enters the dangerous world of drugs and crime.",
+        "poster_path": "/1yeVJox3rjo2jBKrrihIMj7uoS9.jpg"
+      }
+    ]
+  };
+
+  group("Movie list creation test", () {
+    test('Get movies now playing', () async {
+      final apiProvider = TMDBApiProvider();
+      apiProvider.client = MockClient((request) async {
+        return Response(json.encode(moviesJson), 200);
+      });
+      final movies = await apiProvider.fetchNowPlayingMovieList();
+      expect(movies.results[0].id, 450465);
     });
-    final item = await apiProvider.fetchNowPlayingMovieList();
-    expect(item.results[0].id, 450465);
+
+    test('Get popular movies', () async {
+      final apiProvider = TMDBApiProvider();
+      apiProvider.client = MockClient((request) async {
+        return Response(json.encode(moviesJson), 200);
+      });
+      final movies = await apiProvider.fetchPopularMovieList();
+      expect(movies.results[0].originalTitle, 'Glass');
+    });
+
+    test('Get upcoming movies', () async {
+      final apiProvider = TMDBApiProvider();
+      apiProvider.client = MockClient((request) async {
+        return Response(json.encode(moviesJson), 200);
+      });
+      final movies = await apiProvider.fetchUpcomingMovieList();
+      expect(movies.results[0].voteCount, 1031);
+    });
+  });
+
+  group("TV Shows list creation test", () {
+    test('Get popular tv shows', () async {
+      final apiProvider = TMDBApiProvider();
+      apiProvider.client = MockClient((request) async {
+        return Response(json.encode(tvsJson), 200);
+      });
+      final tvshows = await apiProvider.fetchPopularTVShows();
+      expect(tvshows.results[0].id, 1396);
+    });
+
+    test('Get trending tv shows', () async {
+      final apiProvider = TMDBApiProvider();
+      apiProvider.client = MockClient((request) async {
+        return Response(json.encode(tvsJson), 200);
+      });
+      final tvshows = await apiProvider.fetchTrendingTVShows();
+      expect(tvshows.results[0].name, 'Breaking Bad');
+    });
+
+    test('Get top rated tv shows', () async {
+      final apiProvider = TMDBApiProvider();
+      apiProvider.client = MockClient((request) async {
+        return Response(json.encode(tvsJson), 200);
+      });
+      final tvshows = await apiProvider.fetchTopRatedTVShows();
+      expect(tvshows.results[0].originalName, 'Breaking Bad');
+    });
+    
   });
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,8 +9,52 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:nextflix_test1/src/app.dart';
+import 'package:nextflix_test1/src/ui/widgets/carousel.dart';
+import 'dart:convert';
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
+import 'package:nextflix_test1/src/resources/tmdb_api_provider.dart';
+import 'package:nextflix_test1/src/resources/config.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() {
+  Config config = new Config();
+  final tvsJson = {
+    "results": [
+      {
+        "original_name": "Breaking Bad",
+        "genre_ids": [18],
+        "name": "Breaking Bad",
+        "popularity": 88.59,
+        "origin_country": ["US"],
+        "vote_count": 3076,
+        "first_air_date": "2008-01-20",
+        "backdrop_path": "/eSzpy96DwBujGFj0xMbXBcGcfxX.jpg",
+        "original_language": "en",
+        "id": 1396,
+        "vote_average": 8.4,
+        "overview":
+            "When Walter White, a New Mexico chemistry teacher, is diagnosed with Stage III cancer and given a prognosis of only two years left to live. He becomes filled with a sense of fearlessness and an unrelenting desire to secure his family's financial future at any cost as he enters the dangerous world of drugs and crime.",
+        "poster_path": "/1yeVJox3rjo2jBKrrihIMj7uoS9.jpg"
+      },
+      {
+        "original_name": "Stranger Things",
+        "genre_ids": [18, 9648, 10765],
+        "name": "Stranger Things",
+        "popularity": 50.399,
+        "origin_country": ["US"],
+        "vote_count": 2075,
+        "first_air_date": "2016-07-15",
+        "backdrop_path": "/56v2KjBlU4XaOv9rVYEQypROD7P.jpg",
+        "original_language": "en",
+        "id": 66732,
+        "vote_average": 8.3,
+        "overview":
+            "When a young boy vanishes, a small town uncovers a mystery involving secret experiments, terrifying supernatural forces, and one strange little girl.",
+        "poster_path": "/vNJ95BMCzIBJZhW0IEoxzOoW53P.jpg"
+      }
+    ]
+  };
   testWidgets('Detail page navigation smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(NextFlix());
@@ -26,5 +70,35 @@ void main() {
     // Verify that our counter has incremented.
     expect(find.text('TRAILER'), findsNothing);
     expect(find.text('EN CARTELERA'), findsOneWidget);
+  });
+
+  testWidgets('Verify TV Show Carousel generation',
+      (WidgetTester tester) async {
+    final apiProvider = TMDBApiProvider();
+    apiProvider.client = MockClient((request) async {
+      return Response(json.encode(tvsJson), 200);
+    });
+    final tvshows = await apiProvider.fetchPopularTVShows();
+
+    await tester.pumpWidget(FlixCarousel(
+      items: tvshows.results,
+      cover: false,
+      isTv: true,
+    ));
+
+    print('${config.imageUrl}${tvshows.results[0].posterPath}');
+
+    expect(find.widgetWithText(CarouselSlider, '${tvshows.results[1].name}'), findsOneWidget);
+
+    /* expect(
+        find.byWidget(
+          Image.network(
+            '${config.imageUrl}${tvshows.results[0].posterPath}',
+            fit: BoxFit.contain,
+            width: 1000.0,
+          ),
+        ),
+        findsOneWidget); */
+
   });
 }
